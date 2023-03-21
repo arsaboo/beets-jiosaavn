2c32bef76875027bbd1d3c9ff069f6c067f346d2
Alok Saboo <arsaboo@users.noreply.github.com>
2023-03-21 12:23:23 -0400

    Add track import (#2)
    Add track import

--- beetsplug/jiosaavn.py
+++ beetsplug/jiosaavn.py
@@ -6,7 +6,7 @@ import collections
 import re
 
 from beets.autotag.hooks import AlbumInfo, Distance, TrackInfo
-from beets.plugins import BeetsPlugin
+from beets.plugins import BeetsPlugin, get_distance
 from musicapy.saavn_api.api import SaavnAPI
 
 
@@ -18,13 +18,26 @@ class JioSaavnPlugin(BeetsPlugin):
         self.config.add({
             'source_weight': 0.5,
         })
+
     def album_distance(self, items, album_info, mapping):
+
         """Returns the album distance.
         """
         dist = Distance()
         if album_info.data_source == 'JioSaavn':
             dist.add('source', self.config['source_weight'].as_number())
         return dist
+
+    def track_distance(self, item, track_info):
+
+        """Returns the JioSaavn source weight and the maximum source weight
+        for individual tracks.
+        """
+        return get_distance(
+            data_source=self.data_source,
+            info=track_info,
+            config=self.config
+        )
 
     jiosaavn = SaavnAPI()
     def get_albums(self, query):
@@ -51,8 +64,32 @@ class JioSaavnPlugin(BeetsPlugin):
             albums.append(album_info)
         return albums
 
+    def get_tracks(self, query):
+        """Returns a list of TrackInfo objects for a JioSaavn search query.
+        """
+        # Strip non-word characters from query. Things like "!" and "-" can
+        # cause a query to return no results, even if they match the artist or
+        # album title. Use `re.UNICODE` flag to avoid stripping non-english
+        # word characters.
+        query = re.sub(r'(?u)\W+', ' ', query)
+        # Strip medium information from query, Things like "CD1" and "disk 1"
+        # can also negate an otherwise positive result.
+        query = re.sub(r'(?i)\b(CD|disc)\s*\d+', '', query)
+        tracks = []
+        self._log.debug('Searching JioSaavn for: {}', query)
+        try:
+            data = self.jiosaavn.search_song(query)
+        except Exception as e:
+            self._log.debug('Invalid Search Error: {}'.format(e))
+        for track in data["results"]:
+            id = self.jiosaavn.create_identifier(track["perma_url"], 'song')
+            song_details = self.jiosaavn.get_song_details(id)
+            song_info = self._get_track(song_details["songs"][0])
+            tracks.append(song_info)
+        return tracks
+
     def candidates(self, items, artist, release, va_likely, extra_tags=None):
-        """Returns a list of AlbumInfo objects for beatport search results
+        """Returns a list of AlbumInfo objects for JioSaavn search results
         matching release and artist (if not various).
         """
         if va_likely:
@@ -61,6 +98,17 @@ class JioSaavnPlugin(BeetsPlugin):
             query = f'{release} {artist}'
         try:
             return self.get_albums(query)
+        except Exception as e:
+            self._log.debug('JioSaavn Search Error: {}'.format(e))
+            return []
+
+    def item_candidates(self, item, artist, title):
+        """Returns a list of TrackInfo objects for JioSaavn search results
+        matching title and artist.
+        """
+        query = f'{title} {artist}'
+        try:
+            return self.get_tracks(query)
         except Exception as e:
             self._log.debug('JioSaavn Search Error: {}'.format(e))
             return []
@@ -108,15 +156,23 @@ class JioSaavnPlugin(BeetsPlugin):
     def _get_track(self, track_data):
         """Convert a JioSaavn song object to a TrackInfo object.
         """
+        if track_data['duration']:
+            length = int(track_data['duration'].strip())
+        elif track_data['more_info']['duration']:
+            length = int(track_data['more_info']['duration'].strip())
+        if track_data['singers'] == "":
+            artist = track_data['music']
+        else:
+            artist = track_data['singers']
         # Get album information for JioSaavn tracks
         return TrackInfo(
             title=track_data['song'],
             track_id=track_data['id'],
             jiosaavn_track_id=track_data['id'],
-            artist=track_data['singers'],
+            artist=artist,
             album=track_data['album'],
             jiosaavn_artist_id=track_data["music_id"],
-            length=int(track_data['duration']),
+            length=length,
             data_source=self.data_source,
             jiosaavn_perma_url=track_data['perma_url'],
         )
